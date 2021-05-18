52a85f8e51e648df6eb9a7f02b4385ee342e9e3f
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-05-17 22:05:34 -0700

    sockets broken

--- client/src/pages/PublicView.js
+++ client/src/pages/PublicView.js
@@ -1,26 +1,44 @@
 import React, { Fragment, useState, useEffect } from 'react'
-// import io from 'socket.io-client'
-
 import { Link } from 'react-router-dom'
+import io from 'socket.io-client'
 import './PublicView.css'
 
 
-// const socket = io.connect('http://localhost:4000')
+const socket = io.connect('http://localhost:4000')
 
 const PublicView = () => {
     const [password, setPassword] = useState("")
-    const [state, setState] = useState({ name: "", grade: "", description: "" })
-    const [submissions, setSubmission] = useState([])
+    const [state, setState] = useState({ name: "", description: "" })
+    const [submissions, setSubmissions] = useState([])
+    let adminpass = "admin123"
+
+    const renderAdminLogin = () => {
+        if (password == adminpass) {
+            return (
+                <Link to="/admin">
+                    <button type="button" className="btn btn-secondary">Sumbit</button>
+                </Link>
+            )
+        } else {
+            return (
+                //Default text that appears until correct admin password is entered
+                <button type="button" className="btn btn-secondary disabled" > Incorrect Password... </button >
+            )
+        }
+    }
+
 
     return (
         <Fragment>
             <div className="top-queue-container">
                 {/* Header */}
-                <h1 className="queue-title text-center mt-5">Welcome to the Queue</h1>
+                <h1 h1 className="queue-title text-center mt-5" > Welcome to the Queue</h1>
                 <div className="d-flex mt-5">
+                    {/* Create Submission Btn */}
                     <button type="button" className="btn btn-secondary mx-3" data-bs-toggle="modal" data-bs-target="#submissionsModal">
                         Create Submission
                     </button>
+                    {/* Submissions Modal */}
                     <div className="modal fade" id="submissionsModal" tabIndex="-1" aria-hidden="true">
                         <div className="modal-dialog">
                             <div className="modal-content">
@@ -30,6 +48,8 @@ const PublicView = () => {
                                 </div>
                                 <div className="modal-body">
                                     <form>
+                                        {/*Text inside Modal that appears when "Create Submissions" Button is Pressed*/}
+
                                         <input className="form-control form-control-lg" type="text" name="name" placeholder="Name" />
                                         <input className="form-control form-control-lg" type="text" name="grade" placeholder="Grade" />
                                         <input className="form-control form-control-lg" type="text" name="description" placeholder="Description" />
@@ -41,9 +61,11 @@ const PublicView = () => {
                             </div>
                         </div>
                     </div>
+                    {/* Admin Login Btn */}
                     <button type="button" className="btn btn-danger ml-3" data-bs-toggle="modal" data-bs-target="#adminModal">
                         <i class="fas fa-lock"></i>
                     </button>
+                    {/* Admin Login Modal */}
                     <div className="modal fade" id="adminModal" tabIndex="-1" aria-hidden="true">
                         <div className="modal-dialog">
                             <div className="modal-content">
@@ -56,14 +78,14 @@ const PublicView = () => {
                                         <input className="form-control form-control-default" onChange={e => setPassword(e.target.value)} type="password" placeholder="Admin Password" />
                                     </form>
                                 </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary">Join Admin Queue</button>
+                                <div className="modal-footer ">
+                                    {renderAdminLogin()}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
             <div className="bottom-queue-container">
                 {/* Submissions */}
                 <table id="rows" className="table table-striped table-dark text-center">
@@ -75,7 +97,7 @@ const PublicView = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {submissions.map(({ name, grade, description }, index) => (
+                        {submissions.map((index) => (
                             <Fragment>
                                 <tr key={index}>
                                     <td>{submissions.name}</td>
@@ -88,7 +110,7 @@ const PublicView = () => {
                     </tbody>
                 </table>
             </div>
-        </Fragment>
+        </Fragment >
     )
 }
 
